7824c188b0b91ce3abf713a7bdd6abcc973a4732
NatalieHumphriesGitHub <natalie.humphries@outlook.com>
2022-04-28 08:01:39 +0000

    removed .git directory from rest clinic

--- jenkins/kubernetes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws eks update-kubeconfig --name project-cluster
-kubectl apply -f kubernetes/frontend.yaml
-kubectl delete pod backend
-kubectl apply -f kubernetes/backend.yaml
-kubectl apply -f kubernetes/mysql-deployment.yaml
